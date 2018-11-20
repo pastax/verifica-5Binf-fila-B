7c5b0a82053db0e9b4f5dd18045f0c372bb95995
Your Name <you@example.com>
2018-11-20 11:27:39 +0100

    //consegna compito

--- code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
+++ code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
@@ -1,7 +1,7 @@
 
 // TODO Inserire il package corretto
 // nota: potete usare il suggerimento della lampadina a sinistra (scegliete l'unica opzione sensata). 
-package __TODO__;
+package it.marconicivitavecchia.as2018_19.c5Binf.verifica;
 
 import javax.swing.*;
 
@@ -12,18 +12,36 @@ public class Main extends JFrame {
 		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
 		// TODO Inserire la size in accordo con il wireframe (valori approssimati)
-		super.setSize(__TODO__,__TODO__);
+		super.setSize(900,600);
 		
 		// TODO Usare StringBuilder per costruire la stringa HTML
-		
-		// TODO Creare la stringa HTML per il wireframe
+		StringBuilder sb= new StringBuilder ();
+		sb.append("<html>");  
+		sb.append("<h1><g> LIVE THE COMPANY</g></h1>"); //imposto il titolo grande e in grassettio
+		sb.append("<table><tr>");			//creo la tabbella e apro la riga
+		sb.append("<td><h3>evento</h3></td>"); //creo varie colonne 
+		sb.append("<td><h3>data</h3></td>");
+		sb.append("<td><h3>vado?</h3></td>");
+		sb.append("</tr><tr>");
+		sb.append("<td>coctail party</td>");
+		sb.append("<td>7dic</td>");
+		sb.append("<td>si</td>");
+		sb.append("</tr><tr>");
+		sb.append("<td>colazione sociale</td>");
+		sb.append("<td>12dic</td>");
+		sb.append("<td>si</td>");
+		sb.append("</tr><tr>");
+		sb.append("<td>saluti di natale</td>");
+		sb.append("<td>23dic</td>");
+		sb.append("<td>no</td>");
+		sb.append("</tr>");
+		sb.append("</html>");
+		String htmlText = sb.toString();
 		
 		// TODO Convertire lo StringBuilder in String
-		
-		// TODO Assegnare correttamente la stringa create a JLabel
-		super.add(new JLabel(__TODO__));
+		super.add(new JLabel(htmlText));
 		super.setVisible(true);
-		 
+		// TODO Assegnare correttamente la stringa create a JLabel
 		System.out.println("Verifica Swing-GitHub");
 		
 		
